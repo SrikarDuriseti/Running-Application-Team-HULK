d36e058cb95b1a501a4ae3336aa9e2014024e0c5
SrikarDuriseti <87365265+SrikarDuriseti@users.noreply.github.com>
2024-05-21 13:23:34 -0500

    Add files via upload

---
+++ hulk_5707.sql
@@ -0,0 +1,7 @@
+select * from hulkdb.personal_details
+
+insert into hulkdb.personal_details values(1001,'Dheeraj','Male',26,75,172,718787557)
+insert into hulkdb.personal_details values(1002,'Srikar','Male','25',70,172,944088572) 
+insert into hulkdb.personal_details values(1003,'Rachna','Female',24,65,165,955852453)
+insert into hulkdb.personal_details values(1004,'Kavya','Female',24,55,163,756494488)
+insert into hulkdb.personal_details values(1005,'Niharika','Female',25,68,169,925452145)
